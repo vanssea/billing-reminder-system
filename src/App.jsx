d355083e2153766f92f3acb761456fdd248e07f0
febriansyahadin <febriansyahhoho@gmail.com>
2026-08-12 16:05:48 +0700

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Billing Reminder System
+      </h1>
+    </div>
+  )
+}
+
+export default App
