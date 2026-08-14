487d4e14510b2de1aff1d9556a7d3279b24f4a4b
febriansyahadin <febriansyahhoho@gmail.com>
2026-08-15 00:11:10 +0700

    feat: complete client CRUD

--- backend/handlers/client_handler.go
+++ backend/handlers/client_handler.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 
 	"billing-reminder-system/services"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type ClientHandler struct {
@@ -27,4 +29,68 @@ func (h *ClientHandler) GetClients(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	json.NewEncoder(w).Encode(clients)
+}
+func (h *ClientHandler) CreateClient(w http.ResponseWriter, r *http.Request) {
+	var req services.CreateClientRequest
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Format JSON tidak valid", http.StatusBadRequest)
+		return
+	}
+
+	client, err := h.Service.CreateClient(req)
+	if err != nil {
+		http.Error(w, "Gagal membuat client: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(client)
+}
+func (h *ClientHandler) GetClientByID(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	client, err := h.Service.GetClientByID(id)
+	if err != nil {
+		http.Error(w, "Client tidak ditemukan", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(client)
+}
+func (h *ClientHandler) UpdateClient(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	var req services.UpdateClientRequest
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Format JSON tidak valid", http.StatusBadRequest)
+		return
+	}
+
+	client, err := h.Service.UpdateClient(id, req)
+	if err != nil {
+		http.Error(w, "Gagal mengupdate client: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(client)
+}
+func (h *ClientHandler) DeleteClient(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	err := h.Service.DeleteClient(id)
+	if err != nil {
+		http.Error(w, "Gagal menghapus client: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
